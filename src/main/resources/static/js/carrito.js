f6a3ef7475d7d27877f6ace88406cb5af99bb005
Abizaic <abiza_12@icloud.com>
2025-05-13 23:02:08 -0600

    feat(frontend): creación del menú de navegación y carrito básico

---
+++ src/main/resources/static/js/carrito.js
@@ -0,0 +1,7 @@
+let carrito = [];
+
+function agregarAlCarrito(producto) {
+  carrito.push(producto);
+  document.getElementById("contador").textContent = carrito.length;
+  console.log("Carrito:", carrito);
+}
